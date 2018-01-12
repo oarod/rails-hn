f88bc3fc813e26a0ee4107864deb64a2aaae554d
Oscar A Rodriguez <oscar@pandastrike.com>
2018-01-12 01:12:40 -0500

    Refactor API fetch

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,6 +3,20 @@ class Story
 
   attr_accessor :id,:title, :points, :user, :time, :time_ago, :comments_count, :type, :url, :domain, :content, :comments
 
+  def self.all
+    stories = JSON.parse open('https://node-hnapi.herokuapp.com/news').read
+
+    stories.map do |story|
+      Story.new story
+    end
+  end
+
+  def self.find(id)
+    story = JSON.parse open("https://node-hnapi.herokuapp.com/item/#{id}").read
+
+    Story.new story
+  end
+
   def initialize(attributes)
     super attributes
 
