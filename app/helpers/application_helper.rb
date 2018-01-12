eef438b637b854e1fa643609d7834121b9542e33
Oscar A Rodriguez <oscar@pandastrike.com>
2018-01-12 14:40:41 -0500

    Implement paging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def current_page
+    (params[:page] || 1).to_i
+  end
+
+  def first_page?
+    current_page == 1
+  end
+
+  def previous_page_path
+    page_params.merge page: current_page - 1
+  end
+
+  def next_page_path
+    page_params.merge page: current_page + 1
+  end
+
+  private
+
+  def page_params
+    params.permit(:page)
+  end
 end
