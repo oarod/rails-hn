7bc86e626400d6aae2d95c5b70d2a4e6dd7a4c90
Oscar A Rodriguez <oscar@pandastrike.com>
2018-01-12 14:54:29 -0500

    Update paging frontend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,27 @@
 module ApplicationHelper
   def current_page
-    (params[:page] || 1).to_i
+    params[:page] || 1
+  end
+
+  def current_category
+    params[:category] || 'news'
   end
 
   def first_page?
-    current_page == 1
+    current_page.to_i == 1
   end
 
   def previous_page_path
-    page_params.merge page: current_page - 1
+    current_path.merge page: current_page.to_i - 1
   end
 
   def next_page_path
-    page_params.merge page: current_page + 1
+    current_path.merge page: current_page.to_i + 1
   end
 
   private
 
-  def page_params
-    params.permit(:page)
+  def current_path
+    params.permit(:category, :page).merge category: current_category
   end
 end
