36e9533259b06cc77673ed33363aa1f4ff284bb0
Bruno Cunha <bruncun@icloud.com>
2018-01-13 09:48:50 -0500

    Update CORS config

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,10 @@ module HnRails
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
   end
